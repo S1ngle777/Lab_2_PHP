2a79e19551c849e222b01ae2ace8533cdeba1e1c
S1ngle777 <nniebogha@gmail.com>
2024-02-08 14:37:51 +0200

    update for homework.php

--- homework.php
+++ homework.php
@@ -22,3 +22,31 @@ $dayOfWeek = date("w");
         <td><?php echo ($dayOfWeek == 2 || $dayOfWeek == 4 || $dayOfWeek == 6) ? '12:00-16:00' : 'Нерабочий день'; ?></td>
     </tr>
 </table>
+
+<br />
+
+<?php
+    $dayOfWeek = date("w");
+
+    if ((date("G")) < 12){
+        $timeOfDay = "Утро";}
+    elseif ((date("G")) < 18) {
+        $timeOfDay = "День";}
+    else { $timeOfDay = "Вечер";}
+
+    switch ($dayOfWeek) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            $greeting = ($timeOfDay == "Утро") ? "Доброе утро!" : (($timeOfDay == "День") ? "Добрый день!" : "Добрый вечер!");
+            break;
+        case 6:
+        case 0:
+            $greeting = "Сегодня выходные! Наслаждайтесь вашим ";
+            $greeting .= ($timeOfDay == "Утро") ? "утром!" : (($timeOfDay == "День") ? "днем!" : "вечером!");
+            break;
+    }
+?>
+    <h1><?php echo $greeting;?></h1>
